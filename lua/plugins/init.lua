36821972e927897bed2fbc66dab940175e2d3ccb
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2026-03-02 17:58:50 -0500

    remove duplicate nvim-cmp plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,10 +10,4 @@ return {
     { "yorickpeterse/vim-paper" },
     { "sainnhe/everforest" },
     {
-	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp"
-    }
 }
